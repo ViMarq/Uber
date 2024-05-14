03996995993162be8cb70dcd0d5312ecf5d8548f
Vitoria <vitoriaoliveira.1472@aluno.saojudas.br>
2024-05-14 20:36:23 -0300

    Atualizacao da base de dados e limpeza inicial

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Bibliotecas
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -6,28 +7,19 @@ import scipy
 from sklearn.ensemble import IsolationForest
 import datetime
 import time
+from IPython.display import display
 
+# Carregando a base
 uber = pd.read_csv("UBER.csv", sep=',')
 
-for date in uber['Date']:
-    date = time.mktime(datetime.datetime.strptime(date, '%Y-%m-%d').timetuple())
-    uber['Date'] = date
+# Removendo valores nulos e valores iguais a 0
+uber = uber.dropna()
+uber = uber[uber['pickup_longitude'] != 0]
+uber = uber[uber['pickup_latitude'] != 0]
+uber = uber[uber['dropoff_longitude'] != 0]
+uber = uber[uber['dropoff_latitude'] != 0]
 
-print(uber['Date'])
-
-uber = uber.rename(columns={
-    'Date': 'Data', 'Open': 'Preco_inicial', 'High': 'Preco_maximo',
-    'Low': 'Preco_menor', 'Close': 'Preco_fechamento',
-    'Adj Close': 'Preco_fechamanto_acao', 'Volume': 'Total'
-})
-
-print(uber.head())
-
-ax = sns.pairplot(uber.loc[:,['Data','Preco_inicial', 'Preco_maximo', 'Preco_menor', 'Preco_fechamento', 'Preco_fechamanto_acao', 'Total']],
-                      y_vars='Total', 
-                      x_vars=['Data','Preco_inicial', 'Preco_maximo', 'Preco_menor', 'Preco_fechamento', 'Preco_fechamanto_acao'],
-                      kind='reg', 
-                      plot_kws={'line_kws':{'color':'red'}})
-ax.fig.suptitle("Dispersão entre as variáveis", fontsize=10, y=1) # o argumento y indica a posição do titulo em relação ao eixo y
-plt.show()
+# Dropando colunas não desejadas, no caso, unnamed e key
+uber.drop(['Unnamed: 0','key'], axis=1, inplace=True)
+display(uber.head())
 
