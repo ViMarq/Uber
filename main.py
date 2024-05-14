b4da369ff69145ea54da7197a96ad9ca9213fa52
gabriellima2321 <limagab0279@gmail.com>
2024-05-14 19:20:59 -0300

    Modify de date.

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ import seaborn as sns
 import numpy as np
 import scipy
 from sklearn.ensemble import IsolationForest
+import datetime
+import time
 
 uber = pd.read_csv("UBER.csv", sep=',')
 
+for date in uber['Date']:
+    date = time.mktime(datetime.datetime.strptime(date, '%Y-%m-%d').timetuple())
+    uber['Date'] = date
+
+print(uber['Date'])
 
 uber = uber.rename(columns={
     'Date': 'Data', 'Open': 'Preco_inicial', 'High': 'Preco_maximo',
@@ -16,9 +23,9 @@ uber = uber.rename(columns={
 
 print(uber.head())
 
-ax = sns.pairplot(uber.loc[:,['Preco_inicial', 'Preco_maximo', 'Preco_menor', 'Preco_fechamento', 'Preco_fechamanto_acao', 'Total']],
+ax = sns.pairplot(uber.loc[:,['Data','Preco_inicial', 'Preco_maximo', 'Preco_menor', 'Preco_fechamento', 'Preco_fechamanto_acao', 'Total']],
                       y_vars='Total', 
-                      x_vars=['Preco_inicial', 'Preco_maximo', 'Preco_menor', 'Preco_fechamento', 'Preco_fechamanto_acao'],
+                      x_vars=['Data','Preco_inicial', 'Preco_maximo', 'Preco_menor', 'Preco_fechamento', 'Preco_fechamanto_acao'],
                       kind='reg', 
                       plot_kws={'line_kws':{'color':'red'}})
 ax.fig.suptitle("Dispersão entre as variáveis", fontsize=10, y=1) # o argumento y indica a posição do titulo em relação ao eixo y
